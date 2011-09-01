417757f342c9df1609387144dc9a5d395c10e28f
Loic GUILLOIS <contact@loicguillois.fr>
2011-09-01 14:20:53 +0200

    first commit : loading hot air balloon model

---
+++ src/org/jflightsimng/Main.java
@@ -0,0 +1,37 @@
+package org.jflightsimng;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
+import com.jme3.light.Light;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Spatial;
+
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Spatial balloon = assetManager.loadModel("Models/Hot_Air_Balloon.j3o");
+        
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
+        rootNode.addLight(sun);
+        
+        rootNode.attachChild(balloon);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
